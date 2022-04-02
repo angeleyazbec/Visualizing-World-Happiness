11a5f11d264d37c2b52eabcee4bd27643ffa4587
danielgarza22 <danielgarza22@gmail.com>
2022-04-02 13:32:49 -0400

    HTML Update 4

--- static/js/geomap.js
+++ static/js/geomap.js
@@ -1,2 +1,13 @@
 // Geomap JavaScript
 
+var myMap = L.map("geomap-id", {
+    center: [33.7490, -84.3880],
+    zoom: 6
+  });
+  
+  // Adding a tile layer (the background map image) to our map:
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
+  // console.log("Hello")
