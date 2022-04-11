740752b9330142af7c01161c9146c386e34366d0
Nadriana100 <nadriana100@gmail.com>
2022-04-10 23:26:44 -0400

    Added the data for the 10 top countries, layers and markers

--- static/js/geomap.js
+++ static/js/geomap.js
@@ -4,107 +4,442 @@
   // BEFORE COVID-19 AND THROUGH COVID-19
   // DATA COUNTRY, HAPPINES LEVEL, AND LOCATION --
   // YEARS TO VISUALIZE 2018, 2019, 2020, AND 2021
-  // HAPPY COUNTRIES
+
+  // HAPPY COUNTRIES 2018
+
   var happy_2018 = [
+{
+country: "1st Finland",
+happiness:	7.632,
+location: [61.92411,	25.748151]
+},
+{
+country: "2nd Norway",
+happiness:	7.594,
+location: [60.472024,	8.468946]
+},
+{
+country: "3rd Denmark",
+happiness:	7.555,
+location: [56.26392,	9.501785]
+},
+{
+country: "4th Iceland",
+happiness:	7.495,
+location: [64.963051,	-19.020835]
+},
+{
+country: "5th Switzerland",
+happiness:	7.487,
+location: [46.818188,	8.227512]
+},
+{
+country: "6th Netherlands",
+happiness:	7.441,
+location: [52.132633,	5.291266]
+},
+{
+country: "7th Canada",
+happiness:	7.328,
+location: [56.130366,	-106.346771]
+},
+{
+country: "8th New Zealand",
+happiness:	7.324,
+location: [-40.900557,	174.885971]
+},
+{
+country: "9th Sweden",
+happiness:	7.314,
+location: [60.128161,	18.643501]
+},
+{
+country: "10th Australia",
+happiness:	7.272,
+location: [-25.274398,	133.775136]
+}
+];
+
+  // UNHAPPY COUNTRIES 2018
+
+var unHappy_2018 = [
+{
+country: "1st Malawi",
+happiness:	3.587,
+location: [-13.254308,	34.301525]
+},
+{
+country: "2nd Haiti",
+happiness:	3.582,
+location: [18.971187,	-72.285215]
+},
+{
+country: "3rd Liberia",
+happiness:	3.495,
+location: [6.428055,	-9.429499]
+},
+{
+country: "4th Syria",
+happiness:	3.462,
+location: [34.802075,	38.996815]
+},
+{
+country: "5th Rwanda",
+happiness:	3.408,
+location: [	-1.940278,	29.873888]
+},
+{
+country: "6th Yemen",
+happiness:	3.355,
+location: 	[15.552727,	48.516388]
+},
+{
+country: "7th Tanzania",
+happiness:	3.303,
+location: [-6.369028,	34.888822]
+},
+{
+country: "8th South Sudan	",
+happiness: 3.254,
+location: [6.876991899999999, 31.306978800000024]
+},
+{
+country: "9th Central African Republic",
+happiness:	3.083,
+location: [	6.611111,	20.939444]
+},
+{
+country: "10th Burundi",
+happiness:	2.905,
+location: [-3.373056,	29.918886]
+},
+];
+
+// HAPPY COUNTRIES 2019
+
+var happy_2019 = [
   {
-  country: "1st Finland in 2018, 2019, 2020, and 2021",
-  happiness: 7.809,
-  location: [61.92411, 25.748151]
+  country: "1st Finland ",
+  happiness:	7.769,
+  location: [	61.92411,	25.748151]
   },
   {
-    country: "2nd - Norway - 2018",
-    happiness:  7.594,
-    location: [60.472024, 8.468946]
+  country: "2nd Denmark",
+  happiness:	7.6,
+  location: [	56.26392,	9.501785]
   },
   {
-  country: "3rd - Denmark - 2018",
-  happiness:  7.555,
-  location: [56.26392,  9.501785]
+  country: "3rd Norway",
+  happiness:	7.554,
+  location: [60.472024,	8.468946]
   },
   {
-  country: "2nd - Denmark - 2019, 2020, and 2021",
-  happiness:  7.6,
-  location: [56.460449, 10.036367]
+  country: "4th Iceland",
+  happiness:		7.494,
+  location: [	64.963051,	-19.020835]
   },
   {
-  country: "3rd Norway - 2019",
-  happiness:  7.554,
-  location: [58.969975, 5.733107]
+  country: "5th Netherlands",
+  happiness:		7.488,
+  location: [52.132633,	5.291266]
   },
   {
-  country: "2nd Denmark - 2020",
-  happiness: 7.646,
-  location: [57.046707, 9.935932]
+  country: "6th Switzerland",
+  happiness: 7.48,
+  location: [46.818188,	8.227512]
   },
   {
-  country: "3rd Switzerland 2020 and 2021",
-  happiness: 7.560,
-  location: [46.818188, 8.227512]
-  }
+  country: "7th Sweden",
+  happiness:		7.343,
+  location: [60.128161,	18.643501]
+  },
+  {
+  country: "8th New Zealand",
+  happiness:	7.307,
+  location: [-40.900557,	174.885971]
+  },
+  {
+  country: "9th Canada",
+  happiness:	7.278,
+  location: [56.130366,	-106.346771]
+  },
+  {
+  country: "10th Austria",
+  happiness:	7.246,
+  location: [47.516231,	14.550072]
+  },
   ];
-  // UNHAPPY COUNTRIES
-  var unHappy_2018 = [
-    {
-      country: "1st Malawi - 2018",
-      happiness:  3.587,
-      location: [-13.254308,  35.301525]
-    },
-    {
-      country: "2nd Haiti - 2018",
-      happiness:  3.582,
-      location: [18.971187, -72.285215]
-    },
-    {
-      country: "3rd Liberia - 2018",
-      happiness:  3.495,
-      location: [6.428055,  -9.429499]
-    },
-    {
-    country: "1st Haiti - 2019",
-    happiness: 3.597,
-    location: [19.934889, -72.830872]
-    },
-    {
-    country: "2nd Botswana - 2019",
-    happiness:  3.488,
-    location: [-22.328474,  24.684866]
-    },
-    {
-    country: "3rd Syria - 2019",
-    happiness: 3.462,
-    location: [34.802075, 38.996815]
-    },
-    {
-    country: "1st India - 2020",
-    happiness: 3.573,
-    location: [20.593684, 78.96288]
-    },
-    {
-    country: "2nd Malawi - 2020",
-    happiness: 3.538,
-    location: [-13.254308, 33.301525]
-    },
-    {
-    country: "3rd Yemen - 2020",
-    happiness: 3.527,
-    location: [15.552727, 44.516388]
-    },
-    {
-      country: "1st Burundi - 2021",
-      happiness: 3.775,
-      location: [-3.373056, 29.918886]
-    },
-    {
-      country: "2nd Yemen - 2021",
-      happiness: 3.658,
-      location: [15.552727, 48.516388]
-    },
-    {
-      country: "3rd Tanzania - 2021",
-      happiness: 3.623,
-      location: [-6.369028, 34.888822]
-    }
-    ];
+  
+// UNHAPPY COUNTRIES 2019 
+ 
+var unHappy_2019 = [
+{
+  country: "1st Haiti",
+  happiness: 3.597,
+  location: [18.971187,	-72.285215]
+},
+{
+  country: "2nd Botswana",
+  happiness:	3.488,
+  location: [-22.328474,	24.684866]
+},
+{
+  country: "3rd Syria",
+  happiness: 3.462,
+  location: [34.802075,	38.996815]
+},
+{
+  country: "4th Malawi",
+  happiness: 	3.41,
+  location: [-13.254308,	34.301525]
+},
+{
+  country: "5th Yemen",
+  happiness: 	3.38,
+  location: [15.552727,	48.516388]
+},
+{
+  country: "6th Rwanda",
+  happiness: 	3.334,
+  location: [-1.940278,	29.873888]
+},
+{
+  country: "7th Tanzania",
+  happiness: 	3.231,
+  location: [-6.369028,	34.888822]
+},
+{
+  country: "8th Afghanistan",
+  happiness: 	3.203,
+  location: [33.93911,	67.709953]
+},
+{
+  country: "9th Central African Republic",
+  happiness: 	3.083,
+  location: [	6.611111,	20.939444]
+},
+{
+  country: "10th South Sudan",
+  happiness: 2.853,
+  location: [	6.876991899999999,	31.306978800000024]
+},
+];
+
+// HAPPY COUNTRIES 2020 
+
+var happy_2020 = [
+{
+country: "1st Finland",
+happiness: 7.809,
+location: [61.92411, 25.748151]
+},
+{
+country: "2nd Denmark",
+happiness: 7.646,
+location: [56.26392, 9.501785]
+},
+{
+country: "3rd Switzerland",
+happiness: 7.560,
+location: [46.818188, 8.227512]
+},
+{
+  country: "4th Iceland",
+  happiness: 7.504,
+  location: [64.963051, -19.020835]
+},
+{
+  country: "5th Norway",
+  happiness: 7.488,
+  location: [60.472024, 8.468946]
+},
+{
+  country: "6th Netherlands",
+  happiness: 7.449,
+  location: [52.132633, 5.291266]
+},
+{
+  country: "7th Sweden",
+  happiness: 7.353,
+  location: [60.128161, 18.643501]
+},
+{
+  country: "8th New Zealand",
+  happiness: 7.300,
+  location: [-40.900557, 174.885971]
+},
+{
+  country: "9th Austria",
+  happiness: 7.294,
+  location: [47.516231, 14.550072]
+},
+{
+  country: "10th Luxembourg",
+  happiness: 7.238,
+  location: [49.815273, 6.129583]
+},
+];
+
+// UNHAPPY COUNTRIES 2020
+
+var unhappy_2020 = [
+{
+country: "1st India",
+happiness: 3.573,
+location: [20.593684, 78.96288]
+},
+{
+country: "2nd Malawi",
+happiness: 3.538,
+location: [-13.254308, 34.301525]
+},
+{
+country: "3rd Yemen",
+happiness: 3.527,
+location: [15.552727, 48.516388]
+},
+{
+country: "4th Botswana",
+happiness: 3.479,
+location: [-22.328474, 24.684866]
+},
+{
+country: "5th Tanzania",
+happiness: 3.476,
+location: [-6.369028, 34.888822]
+},
+{
+country: "6th Central African Republic",
+happiness: 3.476,
+location: [6.611111, 20.939444]
+},
+{
+country: "7th Rwanda",
+happiness: 3.312,
+location: [-1.940278, 29.873888]
+},
+{
+country: "8th Zimbabwe",
+happiness: 3.299,
+location: [-19.015438, 29.154857]
+},
+{
+country: "9th South Sudan",
+happiness: 2.817,
+location: [6.877, 31.307]
+},
+{
+country: "10th Afghanistan",
+happiness: 2.567,
+location: [33.93911, 67.709953]
+},
+];
+
+// HAPPY COUNTRIES 2021
+
+var happy_2021 = [
+{
+  country: "1st Finland",
+  happiness: 7.842,
+  location: [61.92411, 25.748151]
+},
+{
+  country: "2nd Denmark",
+  happiness: 7.620,
+  location: [56.26392, 9.501785]
+},
+{
+  country: "3rd Switzerland",
+  happiness: 7.571,
+  location: [46.818188, 8.227512]
+},
+{
+  country: "4th Iceland",
+  happiness: 7.554,
+  location: [64.963051, -19.020835]
+},
+{
+  country: "5th Netherlands",
+  happiness: 7.464,
+  location: [52.132633, 5.291266]
+},
+{
+  country: "6th Norway",
+  happiness: 7.392,
+  location: [60.472024, 8.468946]
+},
+{
+  country: "7th Sweden",
+  happiness: 7.363,
+  location: [60.128161, 18.643501]
+},
+{
+  country: "8th Luxembourg",
+  happiness: 7.324,
+  location: [49.815273, 6.129583]
+},
+{
+  country: "9th New Zealand",
+  happiness: 7.277,
+  location: [-40.900557, 174.885971]
+},
+{
+  country: "10 Austria",
+  happiness: 7.268,
+  location: [47.516231, 14.550072]
+},
+];
+
+// UNHAPPY COUNTRIES 2021
+
+var unhappy_2021 = [
+{
+  country: "1st Burundi",
+  happiness: 3.775,
+  location: [-3.373056, 29.918886]
+},
+{
+  country: "2nd Yemen",
+  happiness: 3.658,
+  location: [15.552727, 48.516388]
+},
+{
+  country: "3rd Tanzania",
+  happiness: 3.623,
+  location: [-6.369028, 34.888822]
+},
+{
+  country: "4th Haiti",
+  happiness: 3.615,
+  location: [18.971187, -72.285215]
+},
+{
+  country: "5th Malawi",
+  happiness: 3.600,
+  location: [-13.254308, 34.301525]
+},
+{
+  country: "6th Lesotho",
+  happiness: 3.512,
+  location: [-29.609988, 28.233608]
+},
+{
+  country: "7th Botswana",
+  happiness: 3.467,
+  location: []
+},
+{
+  country: "9th Rwanda",
+  happiness: 3.415,
+  location: [-1.940278, 29.873888]
+},
+{
+  country: "10th Zimbabwe",
+  happiness: 3.145,
+  location: [-19.015438, 29.154857]
+},
+];
+
   // CUSTOM ICON FROME THE MOVIE INSIDE OUT
     var happyIcon = L.icon({
       iconUrl: 'static/images/happy.png',
@@ -119,13 +454,24 @@
       iconAnchor:   [16, 37],
       popupAnchor:  [-3, -38]
     });
+
+
+
   // MARKERS TO GET AND CALL THE DATA IN AN ARRAY
-  var happyMarker = [];
-  var unhappyMarker = [];
+  var happyMarker2018 = [];
+  var unhappyMarker2018 = [];
+  var happyMarker2019 = [];
+  var unhappyMarker2019 = [];
+  var happyMarker2020 = [];
+  var unhappyMarker2020 = [];
+  var happyMarker2021 = [];
+  var unhappyMarker2021 = [];
+
+
   // LOOPING OUR DATA, PUSHING INTO MARKERS, ASSIGNE CUSTOME ICONS, AND ADDING A TOOLTIP
   for (var i = 0; i < happy_2018.length; i++) {
     //var happyCountry = happy_2018[i];
-    happyMarker.push(
+    happyMarker2018.push(
     L.marker(happy_2018[i].location,{icon:happyIcon})
     .bindTooltip(`<h3><b>${happy_2018[i].country}<b/></h3>
     <hr> <h3>Happiness Level ${happy_2018[i].happiness}</h3>`)
@@ -133,16 +479,69 @@
   };
     for (var i = 0; i < unHappy_2018.length; i++) {
      // var sadCountry = unHappy_2018[i];
-     unhappyMarker.push(
+     unhappyMarker2018.push(
      L.marker(unHappy_2018[i].location,{icon:sadIcon})
         .bindTooltip(`<h3><b>${unHappy_2018[i].country}<b/></h3>
         <hr> <h3>Happiness Level ${unHappy_2018[i].happiness}</h3>`)
      );
     };
+
+for (var i = 0; i < happy_2019.length; i++) {
+  happyMarker2019.push(
+  L.marker(happy_2019[i].location,{icon:happyIcon})
+  .bindTooltip(`<h3><b>${happy_2019[i].country}<b/></h3>
+  <hr> <h3>Happiness Level ${happy_2019[i].happiness}</h3>`)
+  );
+};
+for (var i = 0; i < unHappy_2019.length; i++) {
+unhappyMarker2019.push(
+L.marker(unHappy_2019[i].location,{icon:sadIcon})
+  .bindTooltip(`<h3><b>${unHappy_2019[i].country}<b/></h3>
+  <hr> <h3>Happiness Level ${unHappy_2019[i].happiness}</h3>`)
+);
+};
+
+for (var i = 0; i < happy_2020.length; i++) {
+happyMarker2020.push(
+L.marker(happy_2020[i].location,{icon:happyIcon})
+.bindTooltip(`<h3><b>${happy_2020[i].country}<b/></h3>
+<hr> <h3>Happiness Level ${happy_2020[i].happiness}</h3>`)
+);
+};
+for (var i = 0; i < unhappy_2020.length; i++) {
+  unhappyMarker2020.push(
+  L.marker(unhappy_2020[i].location,{icon:sadIcon})
+    .bindTooltip(`<h3><b>${unhappy_2020[i].country}<b/></h3>
+    <hr> <h3>Happiness Level ${unhappy_2020[i].happiness}</h3>`)
+  );
+};
+
+for (var i = 0; i < happy_2021.length; i++) {
+  happyMarker2021.push(
+  L.marker(happy_2021[i].location,{icon:happyIcon})
+  .bindTooltip(`<h3><b>${happy_2021[i].country}<b/></h3>
+  <hr> <h3>Happiness Level ${happy_2021[i].happiness}</h3>`)
+  );
+  };
+  for (var i = 0; i < unhappy_2021.length; i++) {
+    unhappyMarker2021.push(
+    L.marker(unhappy_2021[i].location,{icon:sadIcon})
+      .bindTooltip(`<h3><b>${unhappy_2021[i].country}<b/></h3>
+      <hr> <h3>Happiness Level ${unhappy_2021[i].happiness}</h3>`)
+    );
+  };
     //happyMap.addTo(myMap);
     // DEFINING AND ASSIGNING OUR MARKERS AND LAYERS
-    var happyLayer = L.layerGroup(happyMarker);
-    var unhappyLayer = L.layerGroup(unhappyMarker);
+    var happyLayer18 = L.layerGroup(happyMarker2018);
+    var unhappyLayer18 = L.layerGroup(unhappyMarker2018);
+    var happyLayer19 = L.layerGroup(happyMarker2019);
+    var unhappyLayer19 = L.layerGroup(unhappyMarker2019);
+    var happyLayer20 = L.layerGroup(happyMarker2020);
+    var unhappyLayer20 = L.layerGroup(unhappyMarker2020);
+    var happyLayer21 = L.layerGroup(happyMarker2021);
+    var unhappyLayer21 = L.layerGroup(unhappyMarker2021);
+
+
     // MAPS FROM LEAFLET PROVIDER DEMO
     var happyMap=L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       maxZoom: 19,
@@ -160,14 +559,21 @@
       "Unhappy Map": Jawg_Dark
     };
     var overlays = {
-      "Happiness": happyLayer,
-      "Unhappiness": unhappyLayer
+      "Happiness 2018": happyLayer18,
+      "Unhappiness 2018": unhappyLayer18,
+      "Happiness 2019": happyLayer19,
+      "Unhappiness 2019": unhappyLayer19,
+      "Happiness 2020": happyLayer20,
+      "Unhappiness 2020": unhappyLayer20,
+      "Happiness 2021": happyLayer21,
+      "Unhappiness 2021": unhappyLayer21,
     };
+    
     // CREATING OUR MAP WITH THE LAYERS
     var myMap = L.map("geomap-id", {
-      center: [35.917973, 14.409943],
+      center: [40.4, -3.683333],
       zoom: 3,
-      layers: [happyMap, unhappyLayer
+      layers: [happyMap, happyLayer18
       ]
       });
       // GETTING CONTROL OVER OUR LAYERS
